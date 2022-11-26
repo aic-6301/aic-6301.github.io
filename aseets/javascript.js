1d91433e2898274ebac5eda8578a01d32f81927f
りょうま <90131120+RYOUMA1117@users.noreply.github.com>
2022-11-26 11:21:10 +0000

    レスポンシブ対応その2

--- aseets/javascript.js
+++ aseets/javascript.js
@@ -2,4 +2,11 @@ $(function() {
 $('head').append("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>");
 $('head').append("<link rel='stylesheet' href='/aseets/design.css?"+new Date().getTime()+"'>");
 $('#headers').load("/head.html?"+new Date().getTime());
-  });
+});
+
+function nav_close() {
+  $('nav.nav').fadeOut(400);
+}
+function nav_open() {
+  $('nav.nav').fadeIn(400);
+}
