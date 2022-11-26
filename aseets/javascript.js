5908cf7b507ea44ab8c3a697161b3c3ccaf17eab
りょうま <90131120+RYOUMA1117@users.noreply.github.com>
2022-11-26 10:38:16 +0000

    バグ修正

--- aseets/javascript.js
+++ aseets/javascript.js
@@ -1,4 +1,4 @@
 $(function() {
 $('head').append("<link rel='stylesheet' href='/aseets/design.css?"+new Date().getTime()+"'>");
-$('#headers').load("/head.html");
+$('#headers').load("/head.html"+new Date().getTime());
   });
