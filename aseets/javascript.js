e46215a29f410d88bdefae27aac0c28132abb448
りょうま <90131120+RYOUMA1117@users.noreply.github.com>
2022-11-26 10:46:33 +0000

    だからもっとバグ直した

--- aseets/javascript.js
+++ aseets/javascript.js
@@ -1,4 +1,5 @@
 $(function() {
+$('head').append("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>");
 $('head').append("<link rel='stylesheet' href='/aseets/design.css?"+new Date().getTime()+"'>");
 $('#headers').load("/head.html?"+new Date().getTime());
   });
