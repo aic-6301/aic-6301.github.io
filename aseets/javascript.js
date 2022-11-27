d72b242c687c56ea9e6122092b4027c6e5d1dee8
りょうま <90131120+RYOUMA1117@users.noreply.github.com>
2022-11-27 00:23:41 +0000

    リダイレクト関連の変更

--- aseets/javascript.js
+++ aseets/javascript.js
@@ -1,3 +1,9 @@
+if ("http" === location.protocol) {
+  location.href = "https://aicy.aic-group.net"+location.pathname;
+}
+if ("aic-6301.github.io" === location.hostname) {
+  location.href = "https://aicy.aic-group.net"+location.pathname;
+}
 $(function() {
 $('head').append("<link rel='stylesheet' href='https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css'>");
 $('head').append("<link rel='stylesheet' href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200'>");
